c6cba93ef0426a965406f61b063de3ee258d7e6a
souzatharsis <souzatharsis@gmail.com>
2024-10-16 18:25:22 -0300

    update tests so it works on github actions

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import os
 from podcastfy.text_to_speech import TextToSpeech
 
@@ -9,27 +10,29 @@ class TestAudio(unittest.TestCase):
         self.output_dir = "tests/data/audio"
         os.makedirs(self.output_dir, exist_ok=True)
 
-    # def test_text_to_speech_openai(self):
-    #     tts = TextToSpeech(model="openai")
-    #     output_file = os.path.join(self.output_dir, "test_openai.mp3")
-    #     tts.convert_to_speech(self.test_text, output_file)
+    @pytest.mark.skip(reason="Testing edge only on Github Action as it's free")
+    def test_text_to_speech_openai(self):
+        tts = TextToSpeech(model="openai")
+        output_file = os.path.join(self.output_dir, "test_openai.mp3")
+        tts.convert_to_speech(self.test_text, output_file)
 
-    #     self.assertTrue(os.path.exists(output_file))
-    #     self.assertGreater(os.path.getsize(output_file), 0)
+        self.assertTrue(os.path.exists(output_file))
+        self.assertGreater(os.path.getsize(output_file), 0)
 
-    #     # Clean up
-    #     os.remove(output_file)
+        # Clean up
+        os.remove(output_file)
 
-    # def test_text_to_speech_elevenlabs(self):
-    #     tts = TextToSpeech(model="elevenlabs")
-    #     output_file = os.path.join(self.output_dir, "test_elevenlabs.mp3")
-    #     tts.convert_to_speech(self.test_text, output_file)
+    @pytest.mark.skip(reason="Testing edge only on Github Action as it's free")
+    def test_text_to_speech_elevenlabs(self):
+        tts = TextToSpeech(model="elevenlabs")
+        output_file = os.path.join(self.output_dir, "test_elevenlabs.mp3")
+        tts.convert_to_speech(self.test_text, output_file)
 
-    #     self.assertTrue(os.path.exists(output_file))
-    #     self.assertGreater(os.path.getsize(output_file), 0)
+        self.assertTrue(os.path.exists(output_file))
+        self.assertGreater(os.path.getsize(output_file), 0)
 
-    #     # Clean up
-    #     os.remove(output_file)
+        # Clean up
+        os.remove(output_file)
 
     def test_text_to_speech_edge(self):
         tts = TextToSpeech(model="edge")
